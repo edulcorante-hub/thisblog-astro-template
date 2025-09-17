36f1fbd099f1079d2821670603719e3cdd43341e
maisel <maikoltondini@gmail.com>
2025-09-18 01:18:13 +0200

    Fixed tailwindcss + darkmode

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import preact from "@astrojs/preact";
-
-import tailwindcss from "@tailwindcss/vite";
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://edu-astroblog.netlify.app/",
-  integrations: [preact()],
-
-  vite: {
-    plugins: [tailwindcss()]
-  }
+  integrations: [
+    preact(),
+    tailwind(),
+    // ✅ aggiunta integrazione Tailwind
+  ],
 });
